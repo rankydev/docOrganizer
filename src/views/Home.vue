8ccf760b60a38d64456025a4b022a695775965eb
rankydev <rankyyyyy@gmail.com>
2020-10-29 14:46:01 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <ion-button @click="openModal">Open Modal</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonButton, IonContent, IonPage, modalController } from '@ionic/vue';
+import Modal from './SimpleModal.vue'
+
+export default {
+  components: { IonButton, IonContent, IonPage },
+  methods: {
+    async openModal() {
+      const modal = await modalController
+        .create({
+          component: Modal,
+          componentProps: {
+            data: {
+              content: 'New Content',
+            },
+            propsData: {
+              title: 'New title',
+            },
+          },
+        })
+      return modal.present();
+    },
+  },
+}
+</script>
---
+++ src/views/SimpleModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding ">
+      {{ content }}
+      <ion-button @click="closeModal">Close modal</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar, modalController } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Modal',
+  props: {
+    title: { type: String, default: 'Super Modal' },
+  },
+  data() {
+    return {
+      content: 'This is my cool content',
+    }
+  },
+  components: { IonPage, IonContent, IonHeader, IonTitle, IonToolbar },
+  setup() {
+    const closeModal = () => {
+      modalController.dismiss();
+    };
+    return {
+      closeModal
+    }
+  }
+});
+</script>
